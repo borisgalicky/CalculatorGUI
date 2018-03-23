d390671bd39a03987053ad203fb96f035daf0b51
borisgalicky <boris.galicky@akademiasovy.sk>
2018-03-23 09:40:56 +0100

    improved  design, fixed bugs and added new features

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,6 +8,7 @@ import javafx.scene.control.TextField;
 public class Controller{
     Float firstOperand = 0f;
     int operation = 0;
+
     @FXML
     public Button clear;
     @FXML
@@ -42,9 +43,17 @@ public class Controller{
     private TextField text;
     @FXML
     private Button equal;
+    @FXML
+    private Button two_exp;
+    @FXML
+    private Button three_exp;
+    @FXML
+    private Button dot;
 
     public void process(ActionEvent event) {
-        if (event.getSource() == one) {
+        if(event.getSource() == dot){
+            text.setText(text.getText() + ".");
+        } else if (event.getSource() == one) {
             text.setText(text.getText() + "1");
         } else if (event.getSource() == two) {
             text.setText(text.getText() + "2");
@@ -64,12 +73,27 @@ public class Controller{
             text.setText(text.getText() + "9");
         } else if (event.getSource() == zero) {
             text.setText(text.getText() + "0");
+        } else if (event.getSource() == three_exp) {
+            firstOperand = Float.parseFloat(text.getText());
+            operation = 6;
+            divide.setDisable(true);plus.setDisable(true);minus.setDisable(true);multiply.setDisable(true);two_exp.setDisable(true);three_exp.setDisable(true);one.setDisable(true);
+            two.setDisable(true);three.setDisable(true);four.setDisable(true);five.setDisable(true);six.setDisable(true);seven.setDisable(true);eight.setDisable(true);
+            nine.setDisable(true);zero.setDisable(true);dot.setDisable(true);equal.setDisable(false);
+        } else if (event.getSource() == two_exp) {
+            firstOperand = Float.parseFloat(text.getText());
+            operation = 5;
+            divide.setDisable(true);plus.setDisable(true);minus.setDisable(true);multiply.setDisable(true);two_exp.setDisable(true);three_exp.setDisable(true);one.setDisable(true);
+            two.setDisable(true);three.setDisable(true);four.setDisable(true);five.setDisable(true);six.setDisable(true);seven.setDisable(true);eight.setDisable(true);
+            nine.setDisable(true);zero.setDisable(true);dot.setDisable(true);equal.setDisable(false);
         } else if (event.getSource() == clear) {
             text.clear();
             divide.setDisable(false);
             plus.setDisable(false);
             minus.setDisable(false);
             multiply.setDisable(false);
+            two_exp.setDisable(false);
+            three_exp.setDisable(false);
+            equal.setDisable(true);
         } else if (event.getSource() == divide) {
             firstOperand = Float.parseFloat(text.getText());
             operation = 4;
@@ -77,6 +101,9 @@ public class Controller{
             plus.setDisable(true);
             minus.setDisable(true);
             multiply.setDisable(true);
+            two_exp.setDisable(true);
+            three_exp.setDisable(true);
+            equal.setDisable(false);
             text.setText("");
         } else if (event.getSource() == multiply) {
             firstOperand = Float.parseFloat(text.getText());
@@ -85,6 +112,9 @@ public class Controller{
             plus.setDisable(true);
             minus.setDisable(true);
             multiply.setDisable(true);
+            two_exp.setDisable(true);
+            three_exp.setDisable(true);
+            equal.setDisable(false);
             text.setText("");
         } else if (event.getSource() == minus) {
             firstOperand = Float.parseFloat(text.getText());
@@ -93,6 +123,9 @@ public class Controller{
             plus.setDisable(true);
             minus.setDisable(true);
             multiply.setDisable(true);
+            two_exp.setDisable(true);
+            three_exp.setDisable(true);
+            equal.setDisable(false);
             text.setText("");
         } else if (event.getSource() == plus) {
             firstOperand = Float.parseFloat(text.getText());
@@ -101,6 +134,9 @@ public class Controller{
             plus.setDisable(true);
             minus.setDisable(true);
             multiply.setDisable(true);
+            two_exp.setDisable(true);
+            three_exp.setDisable(true);
+            equal.setDisable(false);
             text.setText("");
         } else if (event.getSource() == equal) {
             Float secondOperand = Float.parseFloat(text.getText());
@@ -131,11 +167,20 @@ public class Controller{
                     }
                     text.setText(String.valueOf(ans));
                     break;
+
+                case 5:
+                    ans = firstOperand * firstOperand;
+                    text.setText(String.valueOf(ans));
+                    break;
+
+                case 6:
+                    ans = firstOperand * firstOperand * firstOperand;
+                    text.setText(String.valueOf(ans));
+                    break;
             }
-            divide.setDisable(false);
-            plus.setDisable(false);
-            minus.setDisable(false);
-            multiply.setDisable(false);
+            divide.setDisable(false);plus.setDisable(false);minus.setDisable(false);multiply.setDisable(false);two_exp.setDisable(false);three_exp.setDisable(false);one.setDisable(false);
+            two.setDisable(false);three.setDisable(false);four.setDisable(false);five.setDisable(false);six.setDisable(false);seven.setDisable(false);eight.setDisable(false);
+            nine.setDisable(false);zero.setDisable(false);dot.setDisable(false);equal.setDisable(true);
         }
     }
 }
